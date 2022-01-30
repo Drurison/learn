00a0758d49e0a42e4b62168fa7ed495e3aad374a
Dagz <jcdagwan@live.com>
2022-01-30 11:00:33 -0500

    Added more tests

--- hello_test.go
+++ hello_test.go
@@ -4,10 +4,24 @@ import "testing"
 
 //Beautiful testing
 func TestHello(t *testing.T) {
-	got := Hello("Joe")
-	want := "Hello, Joe"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Joe")
+		want := "Hello, Joe"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
