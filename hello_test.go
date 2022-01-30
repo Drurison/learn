b332acfe124cb15996af224977e6370185bb34de
Dagz <jcdagwan@live.com>
2022-01-30 11:19:50 -0500

    Added more language tests

--- hello_test.go
+++ hello_test.go
@@ -10,13 +10,24 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Pete", "French")
+		want := "Bonjour, Pete"
+		assertCorrectMessage(t, got, want)
+	})
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Joe")
+		got := Hello("Joe", "")
 		want := "Hello, Joe"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 
