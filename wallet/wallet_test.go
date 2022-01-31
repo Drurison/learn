b66877f03f2a1b5b4bd10deed1e270991dc8920d
Dagz <jcdagwan@live.com>
2022-01-31 15:49:34 -0500

    Started new chapter, pointers and addresses

---
+++ wallet/wallet_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	//"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
